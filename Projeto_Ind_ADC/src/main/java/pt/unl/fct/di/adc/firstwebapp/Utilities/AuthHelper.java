5050cd20c92a0172efe28eee6105f2e0142d78ce
artur67933 <ahc.santos@campus.fct.unl.pt>
2026-07-02 22:28:03 +0100

    bbbbbbb

--- Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/Utilities/AuthHelper.java
+++ Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/Utilities/AuthHelper.java
@@ -24,10 +24,9 @@ public class AuthHelper {
 
 	private AuthHelper() {}
 
-	@SuppressWarnings("unchecked")
 	public static <E extends ModelInterface> E verifyInput(Object obj,Class<E> clas) throws ErrorException {
 		try {
-			return (E) obj;
+			return clas.cast(obj);
 		}catch (Exception e) {
 			try {
 				ErrorException.trow(9929,ModelInterface.formate(clas));
