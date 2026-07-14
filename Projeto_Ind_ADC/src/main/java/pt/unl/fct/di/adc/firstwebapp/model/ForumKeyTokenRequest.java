d9301c337b96ac17ef0d9854bc9831083924287a
Tomás S <tri.silva@campus.fct.unl.pt>
2026-07-14 18:56:37 +0100

    forum post fixed

--- Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/model/ForumKeyTokenRequest.java
+++ Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/model/ForumKeyTokenRequest.java
@@ -1,5 +1,7 @@
 package pt.unl.fct.di.adc.firstwebapp.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class ForumKeyTokenRequest extends AbstractTokenInputRequest<ForumKeyTokenRequest.ForumKeyTokenInput>{
 
 	/**
@@ -15,6 +17,13 @@ public class ForumKeyTokenRequest extends AbstractTokenInputRequest<ForumKeyToke
         private String forumKey;
 
         public ForumKeyTokenInput() {}
+
+        // The client sends "input" as a plain string (the forum key), so accept a bare
+        // JSON string and delegate it to forumKey instead of requiring an object.
+        @JsonCreator
+        public ForumKeyTokenInput(String forumKey) {
+            this.forumKey = forumKey;
+        }
 
         public String getForumKey() {
             return forumKey;
