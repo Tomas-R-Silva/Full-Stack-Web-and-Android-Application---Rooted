6117e9fbd5630adb3e53cd9c0ed3b1cc15a8702e
Gwerreiro <gg26042005+github@gmail.com>
2026-07-15 18:37:21 +0100

    conflict solved

--- Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/Objects/Full.java
+++ Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/Objects/Full.java
@@ -1,9 +1,69 @@
 package pt.unl.fct.di.adc.firstwebapp.Objects;
 
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.LongValue;
+import com.google.cloud.datastore.StringValue;
 
 public interface Full {
 
-	public Map<String,Object> tomap();
+	public Entity toentity();
+	
+	public static String getString(Entity e,String name) {
+		return e.contains(name)?e.getString(name):"";
+	}
+	
+	public static String string(String str) {
+		return str!=null?str:"";
+	}
+	
+	public static long getLong(Entity e,String name) {
+		return e.contains(name)?e.getLong(name):0;
+	}
+	
+	public static double getDouble(Entity e,String name) {
+		return e.contains(name)?e.getDouble(name):0;
+	}
+	
+	public static boolean getBoolean(Entity e,String name) {
+		return e.contains(name)?e.getBoolean(name):true;
+	}
+	
+	public static List<StringValue> getStringValueList(Entity e,String name) {
+		return e.contains(name)?e.getList(name):Collections.emptyList();
+	}
+	
+	public static List<String> getStringList(Entity e,String name){
+		return Full.getStringValueList(e,name).stream().map(v -> v.get()).collect(Collectors.toList());
+	}
+	
+	public static List<StringValue> makeStringValueList(List<String> list) {
+		return (list!=null)?list.stream().map(v -> StringValue.of(v)).collect(Collectors.toList()):Collections.emptyList();
+	}
+	
+	public static List<?> list(List<?> list) {
+		return (list!=null)?list:Collections.emptyList();
+	}
+	
+	public static List<LongValue> makeLongValueList(List<Long> list) {
+		return (list!=null)?list.stream().map(v -> LongValue.of(v)).collect(Collectors.toList()):Collections.emptyList();
+	}
+	
+	public static List<Long> getLongList(Entity e,String name){
+		List<LongValue> l=e.getList(name);
+		return e.contains(name)?l.stream().map(v -> v.get()).collect(Collectors.toList()):Collections.emptyList();
+	}
+	
+	public static <E extends Enum<?>> List<StringValue> makeStringValueEnumList(List<E> list) {
+		return (list!=null)?list.stream().map(v -> StringValue.of(v.name())).collect(Collectors.toList()):Collections.emptyList();
+	}
+	
+	public static <E extends Enum<?>> List<String> makeStringEnumList(List<E> list) {
+		return (list!=null)?list.stream().map(v -> v.name()).collect(Collectors.toList()):Collections.emptyList();
+	}
+	
 	
 }
