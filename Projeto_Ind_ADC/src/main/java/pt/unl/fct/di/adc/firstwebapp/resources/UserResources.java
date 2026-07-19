f1024c3ce51c00fdcecdbf5c9859ad389714810e
artur67933 <ahc.santos@campus.fct.unl.pt>
2026-07-19 17:15:18 +0100

    update /find so ADMINs can find private users

--- Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/UserResources.java
+++ Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/UserResources.java
@@ -231,9 +231,10 @@ public class UserResources {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findAccount(ShortUserTokenRequest request) {
 		try {
-			AuthHelper.verifyToken(request);
-			EntityQuery.Builder queryBuilder = Query.newEntityQueryBuilder().setKind("User").
-					setFilter(PropertyFilter.eq("is_public", true));
+			TokenFull token=AuthHelper.verifyToken(request);
+			EntityQuery.Builder queryBuilder = Query.newEntityQueryBuilder().setKind("User");
+			if(!token.getRole().equals(Role.ADMIN))
+				queryBuilder.setFilter(PropertyFilter.eq("is_public", true));
 			QueryResults<Entity> sessions = datastore.run(queryBuilder.build());
 			List<Map<String,Object>> list=new ArrayList<>();
 			while(sessions.hasNext()) {
