ec50d85c449e3ebb869a3263dfbffad32ea9da83
Gwerreiro <gg26042005+github@gmail.com>
2026-07-19 19:00:54 +0100

    BO dashboard

--- Projeto_Ind_ADC/frontend/src/components/Dashboard-Page/Dashboard-Backoffice.tsx
+++ Projeto_Ind_ADC/frontend/src/components/Dashboard-Page/Dashboard-Backoffice.tsx
@@ -1,9 +1,78 @@
 import NavBar from "../NavBar/NavBar";
+import { useState } from "react";
+import SDGs from "./SDGs";
+import ModerationPartners from "./Moderation-Partners";
+import { useNavigate } from "react-router-dom";
 
 function DashboardBO() {
+  const dashboardItems = ["SDGs", "Moderation (Partners)"];
+  const [selected, setSelected] = useState(dashboardItems[0]);
+
+  const handleSelect = (selection: string) => {
+    setSelected(selection);
+  };
+
+  const navigate = useNavigate();
   return (
     <>
       <NavBar />
+      <div className="container-fluid py-5 px-5">
+        <div className="row g-3">
+          <div className="col-md-2">
+            <div
+              className="rounded-4 h-100 p-3"
+              style={{ background: "var(--color-white)" }}
+            >
+              <div className="flex-column py-3">
+                <a
+                  style={{
+                    color: "var(--color-green)",
+                    fontSize: "16px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => navigate("/account/settings")}
+                >
+                  ← Account Settings
+                </a>
+                <h3 style={{ color: "var(--color-green)" }}>Menu</h3>
+                {dashboardItems.map((item, i) => (
+                  <div
+                    key={item}
+                    className="d-flex justify-content-between align-items-center py-3 border-bottom"
+                    style={{ cursor: "pointer" }}
+                  >
+                    <span
+                      className="fw-semibold"
+                      style={{
+                        color:
+                          selected === item
+                            ? "var(--color-gold)"
+                            : "var(--color-green)",
+                      }}
+                      onClick={() => handleSelect(dashboardItems[i])}
+                    >
+                      {item}
+                    </span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="col-md-10">
+            <div
+              className="rounded-4 p-4 "
+              style={{ background: "var(--color-white)", minHeight: "500px" }}
+            >
+              <h2 className="" style={{ color: "var(--color-green)" }}>
+                Admin Dashboard: {selected}
+              </h2>
+              {selected === dashboardItems[0] && <SDGs />}
+              {selected === dashboardItems[1] && <ModerationPartners />}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
