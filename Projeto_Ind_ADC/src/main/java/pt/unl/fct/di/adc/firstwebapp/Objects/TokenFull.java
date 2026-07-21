61d17568c4c65695dc33e1f35cabb1e44c7bce2e
Tomás S <tri.silva@campus.fct.unl.pt>
2026-07-21 20:18:45 +0100

    Revert "fix TIME_DIVIDER in forum and token"
    This reverts commit c0f606e797780919203b3a674120983367297c9e.

--- Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/Objects/TokenFull.java
+++ Projeto_Ind_ADC/src/main/java/pt/unl/fct/di/adc/firstwebapp/Objects/TokenFull.java
@@ -11,15 +11,16 @@ public class TokenFull extends ShortUser implements Full{
 	private static final long TIME_DIVIDER = 1000L;
 	public static final long EXPIRATION_TIME = 15 * 60;
 
-	private final String jwt; // jwt is the full token, i kept the userName,role... so its easier to debug
-	private final Role role;
-	private final long issuedAt;
-	private final long expiresAt;
+	private String jwt; // jwt is the full token, i kept the userName,role... so its easier to debug
+	private Role role;
+	private long issuedAt;
+	private long expiresAt;
 	private final Key key;
 
+	private TokenFull(Key key) {this.key=key;}
 	
 	public TokenFull(Key key,String jwt, String username, Role role,long issuedAt,long expiresAt) {
-		this.key = key;
+		this(key);
 		this.jwt = jwt;
 		this.username = username;
 		this.role = role;
@@ -27,20 +28,36 @@ public class TokenFull extends ShortUser implements Full{
 		this.expiresAt = expiresAt;
 	}
 
-	public boolean isexpierd() {return expiresAt<System.currentTimeMillis();}
-	public String getJwt() { return jwt; }
-	public Role getRole() { return role; }
-	public String getRoleString() {return role.name(); }
-	public long getIssuedAt() { return issuedAt; }
-	public long getExpiresAt() { return expiresAt; }
+	public TokenFull(Key key,String jwt, String username, Role role) {
+		this(key,jwt, username, role,
+				System.currentTimeMillis() / TIME_DIVIDER,
+				System.currentTimeMillis() / TIME_DIVIDER + EXPIRATION_TIME);
+	}
 
+	public boolean isexpierd() {
+		return (expiresAt*TIME_DIVIDER)<System.currentTimeMillis();
+	}
+
+	public String getJwt() { return jwt; }
+	public void setJwt(String jwt) { this.jwt = jwt; }
+
+	public Role getRole() { return role; }
+	public void setRole(Role role) { this.role = role; }
+	public void setRole(String role) { this.role =Role.valueof(role); }
+	public String getRoleString() {return role.name(); }
+
+	public long getIssuedAt() { return issuedAt; }
+	public void setIssuedAt(long issuedAt) { this.issuedAt = issuedAt; }
+
+	public long getExpiresAt() { return expiresAt; }
+	public void setExpiresAt(long expiresAt) { this.expiresAt = expiresAt; }
 
 	@Override
 	public Map<String,Object> tomap(){
 		return Map.of(
 				"jwt", Full.string(jwt),
-				"username", Full.string(username),
-				"role", Full.string(role.name()),
+				"username", username,
+				"role", role.toString(),
 				"issuedAt", issuedAt*TIME_DIVIDER,
 				"expiresAt", expiresAt*TIME_DIVIDER
 				);
@@ -49,9 +66,9 @@ public class TokenFull extends ShortUser implements Full{
 	@Override
 	public Entity toentity() {
 		return Entity.newBuilder(key)
-				.set("jwt", Full.string(jwt))
-				.set("user_name", Full.string(username))
-				.set("role", Full.string(role.name()))
+				.set("jwt", jwt)
+				.set("user_name", username)
+				.set("role", role.name())
 				.set("issued_at", issuedAt/TIME_DIVIDER)
 				.set("expires_at", expiresAt/TIME_DIVIDER)
 				.build();
